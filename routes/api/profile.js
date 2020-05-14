61258b4b14fa47a24f9f96a58365b3e8896fe2bc
akshitcchopra44 <64258562+akshitcchopra44@users.noreply.github.com>
2020-05-14 14:21:28 +0530

    Post API routes added

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -289,8 +289,20 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
 router.get('/github/:username', async(req, res) => {
   try {
     const options = {
-      uri: ``
-    }
+      uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}`,
+      method: 'GET',
+      headers: { 'user-agent': 'node.js' }
+    };
+
+    request(options, (error, response, body) => {
+      if(error) console.error(error);
+
+      if(response.statusCode !== 200) {
+        res.status(404).json({ msg: 'No github profile found' });
+      }
+
+      res.json(JSON.pparse(body));
+    })
   } catch(err) {
     console.error(err.message);
     res.status(500).send('Server Error');
