894a7c0a50102b1d0ac279264b780224bbe5666c
akshitcchopra44 <64258562+akshitcchopra44@users.noreply.github.com>
2020-05-16 18:12:51 +0530

    React user authentication

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,10 +1,11 @@
 import React, { Fragment, useState } from 'react'
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { setAlert } from '../../actions/alert';
+import { register } from '../../actions/auth';
 import PropTypes from 'prop-types'
 
-const Register = ({ setAlert }) => {
+const Register = ({ setAlert, register, isAuthenticated }) => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -21,6 +22,7 @@ const Register = ({ setAlert }) => {
     if(password !== password2) {
       props.setAlert('Password do not match', 'danger');
     } else {
+      register({ name, email, password });
       // const newUser = {
       //   name,
       //   email,
@@ -41,6 +43,9 @@ const Register = ({ setAlert }) => {
       //   console.error(err.response.data);
       // }
     }
+  }
+  if(isAuthenticated) {
+    return <Redirect to='/dashboard' />;
   }
 
   return (
@@ -86,7 +91,13 @@ const Register = ({ setAlert }) => {
 }
 
 Register.propTypes = {
-  setAlert: PropTypes.func.isRequired
+  setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
 }
 
-export default connect(null, { setAlert })(Register);
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps, { setAlert, register })(Register);
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,18 +1,41 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-const Navbar = () => {
+import React, { Fragment } from 'react'
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { logout } from '../../actions/auth'
+const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
+  const guestLinks = (
+    <ul>
+      <li><a href="#!">Developers</a></li>
+      <li><Link to="/register">Register</Link></li>
+      <li><Link to="/login">Login</Link></li>
+    </ul>
+  );
+
+  const authLinks = (
+    <ul>
+      <li><a onClick={logout} href="#!">
+        <i className='fas fa-sign-out-alt' />{' '}
+        <span className='hide-sm'>Logout</span>
+      </a></li>
+    </ul>
+  );
   return (
     <nav className="navbar bg-dark">
       <h1>
         <Link to="/"><i className="fas fa-code" /> DevConnector</Link>
       </h1>
-      <ul>
-        <li><a href="profiles.html">Developers</a></li>
-        <li><Link to="/register">Register</Link></li>
-        <li><Link to="/login">Login</Link></li>
-      </ul>
+      { !loading && (<Fragment>{ isAuthenticated ? authLinks : guestLinks }</Fragment>)}
     </nav>
   )
 }
 
-export default Navbar;
+Navbar.propTypes = {
+  logout: PropTypes.func.isRequired
+}
+
+const mapStateToProps = state => ({
+  auth: state.auth
+})
+
+export default connect(mapStateToProps, { logout })(Navbar);
