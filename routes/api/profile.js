16cffb1675807754877d50097ffe48cbfc4f36e4
akshitcchopra44 <64258562+akshitcchopra44@users.noreply.github.com>
2020-05-20 13:29:11 +0530

    Dashboard and Profile management

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -7,6 +7,7 @@ const { check, validationResult } = require('express-validator')
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const Post = require('../../models/Post');
 
 // @route    GET api/profile/me
 // @desc     Get current users profile
@@ -135,6 +136,9 @@ router.get('/user/:user_id', async (req, res) => {
 // @access    Private
 router.get('/', auth, async (req, res) => {
   try {
+    // Remove user posts
+    await Post.deleteMany({ user: req.user.id });
+
     // Remove profile
     await Profile.findOneAndRemove({ user: req.user.id });
     // Remove user
